6586d0a4b0cf02097deeed56fac545c9d06f2902
Bruno Augusto Bardelli <brunobardelli@gmail.com>
2021-11-28 20:05:29 +0100

    added region check

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import cherrypy
 import socket
 import boto3
+import requests
+
+'''funtion to get current region'''
+def get_region():
+    r = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document")
+    response_json = r.json()
+    return response_json.get('region')
 
 '''function that shows current hostname'''
 def show_server_hostname():
@@ -8,7 +15,7 @@ def show_server_hostname():
 
 '''Get all items from dynamodb table'''
 def get_all_items():
-    dynamodb = boto3.resource('dynamodb')
+    dynamodb = boto3.resource('dynamodb',region_name=get_region())
     table = dynamodb.Table('application_table')
     response = table.scan()
     return response['Items']
