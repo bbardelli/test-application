376120b88eb69a1a27c134ba269f87d3736161a1
Bruno Augusto Bardelli <brunobardelli@gmail.com>
2021-11-28 19:32:05 +0100

    added dynamodb

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import cherrypy
 import socket
+import boto3
 
 '''function that shows current hostname'''
 def show_server_hostname():
     return socket.gethostname()
 
-
+'''Get all items from dynamodb table'''
+def get_all_items():
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('application_table')
+    response = table.scan()
+    return response['Items']
 
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
-        return show_server_hostname()
+        output_text = "Hostname:" + show_server_hostname() +"<br>\n"
+        output_text += "Items:" + str(get_all_items())        
+        return output_text
 
 cherrypy.server.socket_host = '0.0.0.0'
 cherrypy.quickstart(HelloWorld())
